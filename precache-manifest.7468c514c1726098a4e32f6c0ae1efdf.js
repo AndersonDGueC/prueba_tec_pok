196f00c48e40f773ab4a4d27cd33cc634954a397
anderson <adguerreroc@unal.edu.co>
2020-06-04 00:07:14 -0500

    Updates

---
+++ precache-manifest.7468c514c1726098a4e32f6c0ae1efdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b64861848dd4299da7bce6e6d9cbc4",
+    "url": "/prueba_tec_pok/index.html"
+  },
+  {
+    "revision": "2fb7c84ce247923ae68b",
+    "url": "/prueba_tec_pok/static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "2fb7c84ce247923ae68b",
+    "url": "/prueba_tec_pok/static/js/2.ef9743ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prueba_tec_pok/static/js/2.ef9743ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03f4760e0baf824631f5",
+    "url": "/prueba_tec_pok/static/js/main.dd998dc9.chunk.js"
+  },
+  {
+    "revision": "688a158a25b372348ab5",
+    "url": "/prueba_tec_pok/static/js/runtime-main.8dabe542.js"
+  }
+]);
